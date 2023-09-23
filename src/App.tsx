2ffac8c6267a155db3a7058517b82324f214632b
vineet <vineetchuadhary45@gmail.com>
2023-09-23 15:34:09 +0530

    first

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import {Inject, ScheduleComponent,Day,Week,WorkWeek,Month,Agenda,EventSettingsModel,ViewsDirective,ViewDirective  } from '@syncfusion/ej2-react-schedule';
+import {DataManager,WebApiAdaptor} from '@syncfusion/ej2-data';
 function App() {
+  const localData:EventSettingsModel={
+    dataSource:[
+      {
+        EndTime:new Date(2023,0,11,6,30),
+        StartTime:new Date(2023,0,11,4,0),
+        Subject:'',
+        isAllDay:true,
+        RecurrenceRule : 'FREQ=DAILY;INTERVAL=1;COUNT=10',
+      }
+    ],
+    fields:{
+      subject:{name:'Summary',default:'No title is provided.'},
+      startTime:{name:'Start'},
+      endTime:{name:'End'},
+    }
+  };
+  const remoteData=new DataManager({
+    url:'https://js.syncfusion.com/demos/ejservices/api/Schedule/loadData',
+    adaptor: new WebApiAdaptor(),
+    crossDomain: true,
+  });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ScheduleComponent currentView='Week' selectedDate={new Date(2023,9,23)} eventSettings={{dataSource:remoteData}}>
+        <ViewsDirective>
+          <ViewDirective option='Day'></ViewDirective>
+          <ViewDirective option='Week'></ViewDirective>
+          <ViewDirective option='TimelineDay'></ViewDirective>
+          <ViewDirective option='TimelineDay'></ViewDirective>
+        </ViewsDirective>
+        <Inject services={[Day,Week,WorkWeek,Month,Agenda]} />
+      </ScheduleComponent>
     </div>
   );
 }
